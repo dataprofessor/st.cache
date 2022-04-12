d1ccd7de1a265572f1fb3811429885246366f65c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-12 15:37:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,14 @@ def load_data_a():
     np.random.rand(10000, 5),
     columns=['a', 'b', 'c', 'd', 'e']
   )
-  return df
+  return st.write(df)
 
 def load_data_b():
   df = pd.DataFrame(
     np.random.rand(10000, 5),
     columns=['a', 'b', 'c', 'd', 'e']
   )
-  return df
+  return st.write(df)
 
 a0 = time()
 load_data_a()
