f9742c24d60a7daaa324b54067e572575ed9fd08
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-12 15:10:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas_profiling
 
 from streamlit_pandas_profiling import st_profile_report
 
-@st.cache
+@st.cache(suppress_st_warning=True)
 def pp_report(input_data):
   df = pd.read_csv(input_data)
   pr = df.profile_report()
