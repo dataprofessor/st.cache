3e1eb39302dc51c412a23080e5db84e2e9a279d0
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-12 15:42:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ st.header('Streamlit Cache')
 
 @st.cache(suppress_st_warning=True)
 def load_data_a():
-  st.subheader('Using st.cache')
   df = pd.DataFrame(
     np.random.rand(10000, 5),
     columns=['a', 'b', 'c', 'd', 'e']
@@ -15,20 +14,26 @@ def load_data_a():
   return st.write(df)
 
 def load_data_b():
-  st.subheader('Not using st.cache')
   df = pd.DataFrame(
     np.random.rand(10000, 5),
     columns=['a', 'b', 'c', 'd', 'e']
   )
   return st.write(df)
 
+# Using cache
 a0 = time()
+
+st.subheader('Using st.cache')
 load_data_a()
+
 a1 = time()
 st.info(a1-a0)
 
-
+# Not using cache
 b0 = time()
+
+st.subheader('Not using st.cache')
 load_data_b()
+
 b1 = time()
 st.info(b1-b0)
