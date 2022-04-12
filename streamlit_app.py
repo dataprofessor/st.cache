1431d4adeec5cab17e0931a48cb37bee9e761d58
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-12 15:15:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,11 @@ import pandas_profiling
 
 from streamlit_pandas_profiling import st_profile_report
 
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
 @st.cache(suppress_st_warning=True)
 def pp_report(input_data):
-  df = pd.read_csv(input_data)
-  pr = df.profile_report()
+  pr = input_data.profile_report()
   return st_profile_report(pr)
 
-pp_report('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+pp_report(df)
